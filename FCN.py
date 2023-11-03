34a0b4ab394c162b9ccb4ecb43f5d24f07de9bfc
thxkew <thxkew.teacher@gmail.com>
2023-11-03 21:50:31 +0900

    first commit

---
+++ FCN.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+import torch
+
+
+class FCN(nn.Module):
+    def __init__(self, input_size, momentum=0.99, eps=0.001):
+        super(FCN, self).__init__()
+        
+        self.feature = nn.Sequential(  
+
+                                        nn.Conv1d(input_size, 128, kernel_size=8),
+                                        nn.BatchNorm1d(128, momentum=momentum, eps=eps),
+                                        nn.ReLU(),
+                                        
+                                        nn.Conv1d(128, 256, kernel_size=5),
+                                        nn.BatchNorm1d(256, momentum=momentum, eps=eps),
+                                        nn.ReLU(),
+                            
+                                        nn.Conv1d(256, 128, kernel_size=3),
+                                        nn.BatchNorm1d(128, momentum=momentum, eps=eps),
+                                        nn.ReLU(),
+                                        
+                                     )
+        
+        
+        self.fc = nn.Linear(128, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        """
+        Input : x has dimensions [B, C, L]
+        Output : x has dimentions [B, 1]
+
+        Where B: batch size, C: number of channel, L:sequence length
+        """
+        
+        x = self.feature(x)
+        x = torch.mean(x, 2)
+        x = self.fc(x)
+        x = self.sigmoid(x)
+
+        return x
